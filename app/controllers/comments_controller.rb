bc9300b2a02c9e820e3cc216dc1c9ada65e3ecb8
Haris <muhamad_haris@hotmail.com>
2017-09-14 15:48:25 +0800

    Add create method for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,22 @@
 class CommentsController < ApplicationController
 
-	def new
+
+	def edit
 	end
 
 	def create
+		photo = Photo.find(params[photo_id])
+		album.photo.comments.create(photo_params)
+		redirect_to albums_photos_path(albums_id: album.id, photos_id: photo.id)
 	end
 
 	def destroy
 	end
 	
+	private
+
+	def photo_params
+		params.require(:comment).permit(:name, :photo)
+	end
+
 end
