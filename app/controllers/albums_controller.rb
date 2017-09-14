6b7694ccbcf3d1945696a6a6a9f258d636c71f9e
jatalim <jacinta.lim@insead.edu>
2017-09-14 09:57:41 +0800

    New changes to album including validation

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -2,9 +2,13 @@ class AlbumsController < ApplicationController
 
 	def index 
 	@album = Album.all
+	@user = User.all
+	##@user basically shows up blank for index page but shows the actual user who posted in def show. WHY?! 
 	end 
 
 	def show
+	# @user = User.find(params[:id])
+	@user = current_user
 	@album = Album.find(params[:id])
 	end 
 
@@ -14,8 +18,13 @@ class AlbumsController < ApplicationController
 
 	def create
 	album_params = params[:album].permit(:name, :description)
-	@album = Album.create(album_params)
-	redirect_to albums_path(id: @album.id)
+	@album = current_user.albums.create(album_params)
+	if @album.save 
+		redirect_to albums_path(id: @album.id)
+	else 
+		flash.now[:error] = "Please try again. Name cannot be blank"
+		render :new
+	end 
 	end
 
 	def edit
@@ -24,9 +33,13 @@ class AlbumsController < ApplicationController
 
 	def update
 	album_params = params[:album].permit(:name, :description)
-	album = Album.find(params[:id])
-	album.update(album_params)
-	redirect_to albums_path(id: album.id)
+	@album = Album.find(params[:id])
+	if @album.update(album_params)
+		redirect_to albums_path(id: @album.id)
+	else 
+		flash.now[:error] = "Please try again. Name cannot be blank"
+		render :edit
+	end 
 	end
 
 	def destroy
--- app/models/album.rb
+++ app/models/album.rb
@@ -1,5 +1,7 @@
 class Album < ApplicationRecord
+validates_presence_of :name
 belongs_to :user, optional: true
 has_many :photos
+
 
 end
